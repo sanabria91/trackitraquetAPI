e5bf2c8a610625a80d059f92a57312471be9f6e7
Kevin Sanabria <sanabriakevin@hotmail.com>
2018-11-04 01:12:39 -0500

    add data migration for user,league and sport tables

---
+++ database/migrations/2018_11_04_031325_create_leagues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leagues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('sport_id');
+            $table->string('name');
+            $table->tinyInteger('is_public');
+            $table->integer('created_by_id');
+            $table->integer('admin_id');
+            $table->timestamps();
+
+            $table->foreign('sport_id')->references('id')->on('sports')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leagues');
+    }
+}
