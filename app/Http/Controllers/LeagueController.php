adb31d130f82f024864a3e43bb3c5c0dae54862e
Kevin Sanabria <sanabriakevin@hotmail.com>
2018-11-04 01:15:47 -0500

    add controller resource for league and sport

---
+++ app/Http/Controllers/LeagueController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\LeagueResource;
+use Illuminate\Http\Request;
+
+class LeagueController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return LeagueResource::collection(League::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return LeagueResource
+     */
+    public function store(Request $request)
+    {
+        $league = League::create([
+            'sport_id' => $request->sport_id,
+            'name' => $request->name,
+            'is_public' => $request->is_public,
+            'created_by_id' => $request->created_by_id,
+            'admin_id' => $request->admin_id
+        ]);
+
+        return new LeagueResource($league);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(League $league)
+    {
+        return new LeagueResource($league);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
